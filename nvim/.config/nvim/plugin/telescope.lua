85ce5666fedacc2f6f89649ae8641fd11ac5323a
Tarun-Kumar07 <tarunkumar00137@gmail.com>
2021-09-20 10:26:05 +0530

    rg installed for grep

--- nvim/.config/nvim/plugin/telescope.lua
+++ nvim/.config/nvim/plugin/telescope.lua
@@ -2,8 +2,10 @@ local map = function(key)
   vim.api.nvim_set_keymap("n",key[1],key[2],{noremap = true})
 end
   
-map { "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>"}
-map { "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>"}
-map { "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>"}
-map { "<leader>fh" ,"<cmd>lua require('telescope.builtin').help_tags()<cr>"}
+map { "<leader>ff", ":lua require('telescope.builtin').find_files()<cr>"}
+map { "<leader>fg", ":lua require('telescope.builtin').git_files()<CR>" }
+map { "<leader>lg", ":lua require('telescope.builtin').live_grep()<cr>"}
+map { "<leader>fb", ":lua require('telescope.builtin').buffers()<cr>"}
+map { "<leader>fh" ,":lua require('telescope.builtin').help_tags()<cr>"}
+map { "<leader>ps", ":lua require('telescope.builtin').grep_string({ search = vim.fn.input('Grep For > ')})<CR>" }
 
