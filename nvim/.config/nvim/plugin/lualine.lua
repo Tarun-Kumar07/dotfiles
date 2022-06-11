aa3ca4f1b8448ef0d7f70907359c3bc3d2a02199
Tarun-Kumar07 <tarunkumar00137@gmail.com>
2022-06-11 23:59:44 +0530

    Setting up lsp left

--- nvim/.config/nvim/plugin/lualine.lua
+++
@@ -1,3 +0,0 @@
-local lualine = require("lualine")
-
-lualine.setup({ options = { theme = "gruvbox-material", global_status = true } })
--- nvim/.config/nvim/plugin/treesitter.lua
+++
@@ -1,6 +0,0 @@
-require("nvim-treesitter.configs").setup({
-	-- Modules and its options go here
-	highlight = { enable = false },
-	incremental_selection = { enable = true },
-	textobjects = { enable = true },
-})
