a47693bd7d1f03f4c5acf6301540e133be0dd151
Tarun Allamsetty <tarun.allamsetty@iongroup.com>
2022-08-06 15:05:42 +0530

    Made configurable for linux (Need to check)

--- nvim/.config/nvim/ftplugin/java.lua
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -4,20 +4,26 @@ if not status then
 end
 
 -- Determine OS
-local home = os.getenv("HOME")
 if vim.fn.has("mac") == 1 then
+  -- TODO : will fill when have mac
   SYSTEM = "mac"
+  HOME = os.getenv("$HOME")
 elseif vim.fn.has("unix") == 1 then
   SYSTEM = "linux"
+  HOME = os.getenv("$HOME")
+  JDTLS_LOCATION = vim.fn.stdpath("data") .. "/lsp_servers/jdtls"
+  LOMBOK_JAR = "/lombok.jar"
+  EQUINOX_JAR = "/plugins/org.eclipse.equinox.launcher_*.jar"
+  CONFIG = "/config"
+  CACHE = "/.cache/"
 else
   SYSTEM = "win"
-end
-
--- Find root of project
-local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
-local root_dir = require("jdtls.setup").find_root(root_markers)
-if root_dir == "" then
-  return
+  HOME = os.getenv("USERPROFILE")
+  JDTLS_LOCATION = vim.fn.stdpath("data") .. "\\lsp_servers\\jdtls"
+  LOMBOK_JAR = "\\lombok.jar"
+  EQUINOX_JAR = "\\plugins\\org.eclipse.equinox.launcher_*.jar"
+  CONFIG = "\\config_"
+  CACHE = "\\.cache\\"
 end
 
 local extendedClientCapabilities = jdtls.extendedClientCapabilities
@@ -25,16 +31,9 @@ extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
 
 local workspace_dir = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
 
-local JDTLS_LOCATION = vim.fn.stdpath("data") .. "\\lsp_servers\\jdtls"
-
--- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
 local config = {
-  -- The command that starts the language server
-  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
   cmd = {
-    -- 💀
-    -- "cmd.exe",
-    "java", -- or '/path/to/java11_or_newer/bin/java'
+    "java",
     "-Declipse.application=org.eclipse.jdt.ls.core.id1",
     "-Dosgi.bundles.defaultStartLevel=4",
     "-Declipse.product=org.eclipse.jdt.ls.core.product",
@@ -44,28 +43,20 @@ local config = {
     "--add-modules=ALL-SYSTEM",
     "--add-opens", "java.base/java.util=ALL-UNNAMED",
     "--add-opens", "java.base/java.lang=ALL-UNNAMED",
-    -- "-javaagent:" .. JDTLS_LOCATION .. "\\lombok.jar",
-    "-jar", vim.fn.glob(JDTLS_LOCATION .. "\\plugins\\org.eclipse.equinox.launcher_*.jar"),
-    "-configuration", JDTLS_LOCATION .. "\\config_" .. SYSTEM,
-    "-data", "%USERPROFILE%\\.cache\\" .. workspace_dir,
+    "-javaagent:" .. JDTLS_LOCATION .. LOMBOK_JAR,
+    "-jar", vim.fn.glob(JDTLS_LOCATION .. EQUINOX_JAR),
+    "-configuration", JDTLS_LOCATION .. CONFIG .. SYSTEM,
+    "-data", HOME .. CACHE .. workspace_dir,
   },
 
   on_attach = require("config.lsp.handlers").on_attach,
   capabilities = require("config.lsp.handlers").capabilities,
 
-  root_dir = root_dir,
+  root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
 
   settings = {
     java = {},
   },
-
-  -- Language server `initializationOptions`
-  -- You need to extend the `bundles` with paths to jar files
-  -- if you want to use additional eclipse.jdt.ls plugins.
-  --
-  -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
-  --
-  -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
 }
 
 require("jdtls").start_or_attach(config)
