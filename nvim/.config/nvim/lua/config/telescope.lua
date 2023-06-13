c548a71549014fb29feec7200cbd4668bb87991b
Tarun-Kumar07 <tarunkumar00137@gmail.com>
2023-06-13 13:54:11 +0530

    Telescope lsp symbols

--- nvim/.config/nvim/lua/config/telescope.lua
+++ nvim/.config/nvim/lua/config/telescope.lua
@@ -1,36 +1,54 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
-	print("Telescope not installed")
-	return
+  print("Telescope not installed")
+  return
+end
+
+local status_ok, themes = pcall(require, "telescope.themes")
+if not status_ok then
+  print("Telescope themes not installed")
+  return
+end
+
+local status_ok, builtin = pcall(require, "telescope.builtin")
+if not status_ok then
+  print("Telescope themes not installed")
+  return
 end
 
 local map = function(key)
-	vim.api.nvim_set_keymap("n", key[1], key[2], { noremap = true })
+  vim.keymap.set("n", key[1], key[2])
 end
 
-map({ "<leader>ff", ":lua require('telescope.builtin').find_files({hidden=true})<cr>" })
-map({ "<leader>fg", ":lua require('telescope.builtin').git_files()<CR>" })
-map({ "<leader>ec", ":lua require('telescope.builtin').find_files({hidden=true,cwd='~/.dotfiles/'})  <CR>" })
-map({ "<leader>lg", ":lua require('telescope.builtin').live_grep()<cr>" })
-map({ "<leader>fb", ":lua require('telescope.builtin').buffers()<cr>" })
-map({ "<leader>fh", ":lua require('telescope.builtin').help_tags()<cr>" })
-map({ "<leader>ps", ":lua require('telescope.builtin').grep_string({ search = vim.fn.input('Grep For > ')})<CR>" })
+map({ "<leader>ff", function() builtin.find_files({ hidden = true }) end })
+map({ "<leader>fg", function() builtin.git_files() end })
+map({ "<leader>lg", function() builtin.live_grep() end })
+map({ "<leader>fb", function() builtin.buffers() end })
+map({ "<leader>fh", function() builtin.help_tags() end })
+map({ "<leader>ps", function() builtin.grep_string({ search = vim.fn.input('Grep For > ') }) end })
+
+--LSP related
+map({ "<leader>fs",
+  function()
+    builtin.lsp_document_symbols({ ignore_symbols = "variable" })
+  end })
 
 telescope.setup({
-	extensions = {
-		["ui-select"] = {
-			require("telescope.themes").get_dropdown({
-				-- even more opts
-			}),
-			-- ["fzf"] = {
-			-- 	fuzzy = true, -- false will only do exact matching
-			-- 	override_generic_sorter = true, -- override the generic sorter
-			-- 	override_file_sorter = true, -- override the file sorter
-			-- 	case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-			-- 	-- the default case_mode is "smart_case"
-			-- },
-		},
-	},
+  pickers = {
+    lsp_document_symbols = { theme = "dropdown" }
+  },
+  extensions = {
+    ["ui-select"] = {
+      themes.get_dropdown(),
+      -- ["fzf"] = {
+      -- 	fuzzy = true, -- false will only do exact matching
+      -- 	override_generic_sorter = true, -- override the generic sorter
+      -- 	override_file_sorter = true, -- override the file sorter
+      -- 	case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+      -- 	-- the default case_mode is "smart_case"
+      -- },
+    },
+  },
 })
 
 -- telescope.load_extension("fzf")
