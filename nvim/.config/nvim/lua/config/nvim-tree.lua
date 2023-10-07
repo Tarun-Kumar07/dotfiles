0a8cc8fad7602f02165770e9fb82ef8399554801
Tarun-Kumar07 <tarunkumar00137@gmail.com>
2023-10-07 17:19:48 +0530

    Open selected file in nvim-tree

--- nvim/.config/nvim/lua/config/nvim-tree.lua
+++ nvim/.config/nvim/lua/config/nvim-tree.lua
@@ -3,6 +3,16 @@ if not status_ok then
 	return
 end
 
+local status_ok, api = pcall(require, "nvim-tree.api")
+if not status_ok then
+	return
+end
+
 nvim_tree.setup()
 
 vim.api.nvim_set_keymap("n", "<Leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function()
+		api.tree.find_file()
+	end,
+})
