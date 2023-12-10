ed23011d5c39efdd38ec4d489d8fa66103999525
Tarun-Kumar07 <tarunkumar00137@gmail.com>
2023-12-10 19:07:20 +0530

    Add catppuccin theme to bufferline

--- nvim/.config/nvim/lua/config/bufferline.lua
+++ nvim/.config/nvim/lua/config/bufferline.lua
@@ -1,39 +1,23 @@
 local status_ok, bufferline = pcall(require, "bufferline")
 if not status_ok then
-  return
+	return
 end
 
 vim.opt.termguicolors = true
 
-bufferline.setup {
-  options = {
-    -- separator_style = "padded_slant",
-    diagnostics = "nvim_lsp",
-    offsets = {
-      {
-        filetype = "NvimTree",
-        text = "File Explorer",
-        text_align = "center",
-        separator = true
-      }
-    },
-  },
-  highlights = {
-    buffer = {
-      fg = "#1d2021",
-      bg = "#1d2021"
-    },
-    separator_selected = {
-      fg = "#1d2021",
-      bg = "#1d2021"
-    },
-    separator_visible = {
-      fg = "#1d2021",
-      bg = "#1d2021",
-    },
-    separator = {
-      fg = "#1d2021",
-      bg = "#1d2021",
-    },
-  }
-}
+bufferline.setup({
+	options = {
+		-- separator_style = "padded_slant",
+		diagnostics = "nvim_lsp",
+		offsets = {
+			{
+				filetype = "NvimTree",
+				text = "File Explorer",
+				text_align = "center",
+				separator = true,
+			},
+		},
+	},
+
+	highlights = require("catppuccin.groups.integrations.bufferline").get(),
+})
