2f6a70a4ac4ef91e07e57132efa9d084df3bad3e
Tarun-Kumar07 <tarunkumar00137@gmail.com>
2023-12-26 17:21:09 +0530

    Add keybindings for incremental selection in treesitter

--- nvim/.config/nvim/lua/config/treesitter.lua
+++ nvim/.config/nvim/lua/config/treesitter.lua
@@ -15,4 +15,13 @@ treesitter_config.setup({
 	indent = {
 		enable = true,
 	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "grn",
+			scope_incremental = "grc",
+			node_decremental = "grm",
+		},
+	},
 })
