c5c7b42bc7479dc58c78aab278f0e28e9547a165
Tarun-Kumar07 <tarunkumar00137@gmail.com>
2025-02-09 13:35:29 +0530

    Add lualine and telescope

---
+++ nvim/.config/nvim/lua/config/plugins/telescope.lua
@@ -0,0 +1,32 @@
+return {
+  'nvim-telescope/telescope.nvim', 
+  tag = '0.1.8',
+  dependencies = { 
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+  },
+  config = function()
+   local builtin = require('telescope.builtin') 
+   vim.keymap.set('n', '<leader>ff', builtin.find_files )
+   vim.keymap.set('n', '<leader>fg', builtin.git_files )
+   vim.keymap.set('n', '<leader>fb', builtin.buffers )
+   vim.keymap.set('n', '<leader>fh', builtin.help_tags )
+   vim.keymap.set('n', '<leader>lg', builtin.live_grep )
+   vim.keymap.set('n', "<leader>ps",
+    function()
+      builtin.grep_string({ search = vim.fn.input("Grep For > "), hidden = true })
+    end
+   )
+
+   require('telescope').setup({
+    defaults = {
+      sorting_strategy = "ascending",
+      layout_statergy = "center",
+      layout_config = {
+        prompt_position = "top",
+      },
+    },
+  })
+
+  end
+}
